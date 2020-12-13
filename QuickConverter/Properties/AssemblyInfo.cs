6c3ffe2c1ebadf32dd73dcf3f0fd6918e435faa4
GF-Huang <GF-Huang@users.noreply.github.com>
2020-12-14 02:44:37 +0800

    Port to .NET Core.

---
+++ QuickConverter/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Windows.Markup;
+
+[assembly: XmlnsDefinition("http://QuickConverter.CodePlex.com/", "QuickConverter")]
+[assembly: XmlnsPrefix("http://QuickConverter.CodePlex.com/", "qc")]
